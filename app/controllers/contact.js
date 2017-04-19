873130b6e868da6350f49aec61fe72a5e6118930
Madhulika Bhadra <madhu.bhadra@gmail.com>
2017-04-18 19:57:11 -0500

    Added contact model

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -11,7 +11,15 @@ export default Ember.Controller.extend({
 
     actions: {
       saveMessage() {
+
+      	const email= this.get('emailAddress');
+      	const message = this.get('myMessage');
+
+      	const newContact = this.store.createRecord('contact', {emailAddress: email, myMessage: message} );
+      	newContact.save();
+
       alert(`Saving of the following message is in progress: ${this.get('myMessage')}`);
+
       this.set('responseMessage', `Thank you! We've just saved your message: ${this.get('myMessage')}`);
       this.set('myMessage', '');
     }
